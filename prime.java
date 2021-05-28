900a50a0487124b679f2a649c6172bc4cea70863
AhmedMohamed21 <82438623+AhmedMohamed21@users.noreply.github.com>
2021-05-28 15:37:36 +0300

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,6 +1,11 @@
 import java.util.scanner;
 class prime{
+  //method for factors
   static void pf(num){
+    for(int i=num ; i >= 2 ; i--)
+      if(n % i == 0){
+        System.out.println("  "+ n / i);
+      }
   }
   public sataic void main(String arg[]){
   pf();
