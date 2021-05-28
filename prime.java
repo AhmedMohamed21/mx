d893888aafac05d25ab6624ffdd2258107c5ccf5
AhmedMohamed21 <82438623+AhmedMohamed21@users.noreply.github.com>
2021-05-28 15:55:15 +0300

    Update prime.java

--- prime.java
+++ prime.java
@@ -3,15 +3,18 @@ class prime{
   //method for factors
   static void pf(num){
     for(int i=num ; i >= 2 ; i--)
-      if(n % i == 0){
-        System.out.println("  "+ n / i);
+      if(num % i == 0){
+        System.out.println("  "+ num / i);
       }
   }
-  public sataic void main(String arg[]){
+  public static void main(String arg[]){
     int n;
     Scanner in=new Scanner(System.in);
     System.out.println("Enter a number \n");
     n=in.nextInt();
+     if(n % 2==0) {
+   	  System.out.println(n+" is composite number and factors are");
   pf(n);
+     }
   }
 }
