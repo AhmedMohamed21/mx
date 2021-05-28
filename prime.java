448ff3aed31cbebe3b3ee615c53f8d9c46bc755c
AhmedMohamed21 <82438623+AhmedMohamed21@users.noreply.github.com>
2021-05-28 23:59:21 +0300

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,5 +1,19 @@
 import java.util.scanner;
 class prime{
+  static	void sum(int num) {
+	int count=0;
+    // System.out.print("Factors of " + num + " are: ");
+
+     for (int i =1; i < num; i++) {
+
+         if (num % i != 0)
+         count=count+1;
+         {
+         }
+
+     }
+        
+              System.out.println("\n iteration is  " + count);}
   //method for factors
   static void pf(num){
     for(int i=num ; i >= 2 ; i--)
@@ -16,7 +30,7 @@ class prime{
    	  System.out.println(n+" is composite number and factors are");
   pf(n);
      }else {   	  System.out.println(n+" is prime number");
-
+        sum(n);
     	
     }
   }
